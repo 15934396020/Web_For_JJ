643e93640a3ed7be795dcfbd92ce9b9314bf1b3d
15934396020 <136323744+15934396020@users.noreply.github.com>
2023-12-21 11:41:58 -0500

    Upgrade for loading animation

--- js/load.js
+++ js/load.js
@@ -1,21 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const progressBar = document.getElementById("progress-bar");
-  const loaderContainer = document.getElementById("loader-container");
-
-  function updateProgressBar() {
-    const scrollPosition = window.scrollY;
-    const windowHeight = window.innerHeight;
-    const bodyHeight = document.body.clientHeight;
-
-    // 计算进度百分比
-    const progress = (scrollPosition / (bodyHeight - windowHeight)) * 100;
-
-    // 设置进度条宽度
-    // progressBar.style.width = `${progress}%`;
-  }
-
-  window.addEventListener("scroll", updateProgressBar);
-  window.addEventListener("resize", updateProgressBar);
+  var loaderContainer = document.getElementById("loader-container");
 
   window.addEventListener("load", function() {
     var load_timer = setInterval(function() {
